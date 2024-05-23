2eb35a4c2d053a44554707411d328d5b1244249f
Haemulramen <apple11078@gmail.com>
2024-05-23 12:20:38 +0900

    FEAT: time change UTC >> Asia/Seoul

--- config/settings.py
+++ config/settings.py
@@ -134,11 +134,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Seoul"
+USE_TZ = False
 
 USE_I18N = True
-
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
